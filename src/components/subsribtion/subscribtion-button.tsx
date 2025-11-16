dae62728a03dc8cf289835dcab25f257e2ec3881
nzaguta <nzaguta@ozon.ru>
2025-11-16 12:17:51 +0300

    Updated logic

--- src/components/subsribtion/subscribtion-button.tsx
+++ src/components/subsribtion/subscribtion-button.tsx
@@ -1,10 +1,10 @@
 import { Popover, PopoverContent, PopoverTrigger } from "@heroui/react";
 import { Button } from "@heroui/button";
 import { useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 import { VkLogo, VkVideoLogo } from "@/components/logos.tsx";
-import { CheckmarkIcon, GoToIcon } from "@/components/icons.tsx";
+import { CheckmarkIcon, CloseIcon, GoToIcon } from "@/components/icons.tsx";
 
 export enum VkMode {
   VkVideo = 0,
@@ -20,6 +20,8 @@ export function SubscribtionButton(props: {
   setIsSecondarySubscribed: (isSecondarySubscribed: boolean) => void;
 }) {
   const [isOpened, setOpened] = useState<boolean>(false);
+  const [isMainSubscribeLoading, setIsMainSubscribeLoading] =
+    useState<boolean>(false);
 
   const onMainSubscribed = () => {
     props.setIsMainSubscribed(true);
@@ -55,96 +57,61 @@ export function SubscribtionButton(props: {
           <motion.div
             layout
             className={
-              "flex flex-col items-center justify-center pt-2 pb-2 pl-1 pr-1 max-w-70"
+              "flex flex-col items-center justify-center pt-2 pb-2 pl-1 pr-1 max-w-80"
             }
           >
             <motion.div
               layout
               className={
-                "grid grid-cols-2 items-center justify-center m-3 gap-3"
+                "grid grid-cols-3 items-center justify-center m-3 gap-3"
               }
-              style={{ gridTemplateColumns: "auto 1fr" }}
+              style={{ gridTemplateColumns: "auto 1fr auto" }}
             >
               <SecondaryLogo vkMode={props.vkMode} />
-              <AnimatePresence mode={"popLayout"}>
-                {props.isSecondarySubscribed ? (
-                  <motion.div
-                    key={"gotoLabel"}
-                    layout
-                    animate={{
-                      x: 0,
-                      opacity: 1,
-                      transition: { ease: "easeInOut" },
-                    }}
-                    className={"w-full"}
-                    initial={{ x: 100, opacity: 0 }}
-                  >
-                    <GoToPopoverLabel
-                      accountName={props.accountName}
-                      vkMode={props.vkMode}
-                    />
-                  </motion.div>
-                ) : (
-                  <motion.div
-                    key={"subscribeLabel"}
-                    layout
-                    animate={{
-                      x: 0,
-                      opacity: 1,
-                      transition: { ease: "easeInOut" },
-                    }}
-                    exit={{ x: -100, opacity: 0 }}
-                  >
-                    <SubscribePopoverLabel
-                      accountName={props.accountName}
-                      vkMode={props.vkMode}
-                    />
-                  </motion.div>
-                )}
-              </AnimatePresence>
+              <motion.div
+                key={"subscribeLabel"}
+                layout
+              >
+                <SubscribePopoverLabel
+                  accountName={props.accountName}
+                  vkMode={props.vkMode}
+                />
+              </motion.div>
+              <Button
+                isIconOnly
+                radius={"full"}
+                size={"sm"}
+                style={{
+                  position: "absolute",
+                  top: 10,
+                  right: 10,
+                  padding: 0,
+                  height: 24,
+                  width: 24,
+                  minWidth: 24,
+                  boxSizing: "content-box",
+                }}
+                onPress={() => setOpened(false)}
+              >
+                <CloseIcon height={10} width={10} />
+              </Button>
             </motion.div>
-            <AnimatePresence mode={"popLayout"}>
-              {props.isSecondarySubscribed ? (
-                <motion.div
-                  key={"gotoAction"}
-                  layout
-                  animate={{
-                    x: 0,
-                    opacity: 1,
-                    transition: { ease: "easeInOut" },
-                  }}
-                  initial={{ x: 100, opacity: 0 }}
-                  style={{
-                    width: "100%",
-                  }}
-                >
-                  <GoToPopoverActions goToVkVideo={goToAccount} />
-                </motion.div>
-              ) : (
-                <motion.div
-                  key={"subscribeAction"}
-                  layout
-                  animate={{
-                    x: 0,
-                    opacity: 1,
-                    transition: { ease: "easeInOut" },
-                  }}
-                  exit={{ x: -100, opacity: 0 }}
-                  style={{
-                    width: "100%",
-                  }}
-                >
-                  <SubscribePopoverActions
-                    onCancel={() => {
-                      setOpened(false);
-                    }}
-                    onSecondarySubscribed={() =>
-                      props.setIsSecondarySubscribed(true)
-                    }
-                  />
-                </motion.div>
-              )}
-            </AnimatePresence>
+            <motion.div
+              key={"subscribeAction"}
+              layout
+              style={{
+                width: "100%",
+              }}
+            >
+              <SubscribePopoverActions
+                vkMode={props.vkMode}
+                onGoto={goToAccount}
+                onSecondarySubscribed={() => {
+                  props.setIsSecondarySubscribed(true);
+                  setOpened(false);
+                }}
+              />
+            </motion.div>
           </motion.div>
         </PopoverContent>
       </Popover>
@@ -170,10 +137,15 @@ export function SubscribtionButton(props: {
           <Button
             className={"w-40 font-medium"}
             color={"secondary"}
+            isLoading={isMainSubscribeLoading}
             variant={"solid"}
             onPress={() => {
-              onMainSubscribed();
-              setOpened(true);
+              setIsMainSubscribeLoading(true);
+              setTimeout(() => {
+                setIsMainSubscribeLoading(false);
+                onMainSubscribed();
+                setOpened(true);
+              }, 1000);
             }}
           >
             Подписаться
@@ -195,67 +167,67 @@ function SubscribePopoverLabel(props: { accountName: string; vkMode: VkMode }) {
     case VkMode.Vk:
       return (
         <p>
-          Подписаться на <AccountLabel accountName={props.accountName} /> в VK
-          Видео?
+          Рекомендовать канал автора{" "}
+          <AccountLabel accountName={props.accountName} /> в VK Видео?
         </p>
       );
   }
 }
 
 function SubscribePopoverActions(props: {
+  vkMode: VkMode;
   onSecondarySubscribed: () => void;
-  onCancel: () => void;
+  onGoto: () => void;
 }) {
+  const [isSecondarySubscribeLoading, setIsSecondarySubscribeLoading] =
+    useState<boolean>(false);
+
+  function getGoToLabel() {
+    switch (props.vkMode) {
+      case VkMode.VkVideo:
+        return "Сообщество";
+      case VkMode.Vk:
+        return "Канал автора";
+    }
+  }
+
+  function getSubscribeLabel() {
+    switch (props.vkMode) {
+      case VkMode.VkVideo:
+        return "Подписаться";
+      case VkMode.Vk:
+        return "Рекомендовать";
+    }
+  }
+
   return (
     <motion.div layout className={"grid grid-cols-2 items-center gap-2 w-full"}>
-      <Button className={"font-medium"} onPress={props.onCancel}>
-        Нет
+      <Button
+        className={"font-medium"}
+        endContent={<GoToIcon height={20} width={20} />}
+        onPress={props.onGoto}
+      >
+        {getGoToLabel()}
       </Button>
       <Button
         className={"font-medium"}
         color={"primary"}
+        isLoading={isSecondarySubscribeLoading}
         variant={"solid"}
-        onPress={props.onSecondarySubscribed}
+        onPress={() => {
+          setIsSecondarySubscribeLoading(true);
+          setTimeout(() => {
+            setIsSecondarySubscribeLoading(false);
+            props.onSecondarySubscribed();
+          }, 1000);
+        }}
       >
-        Подписаться
+        {getSubscribeLabel()}
       </Button>
     </motion.div>
   );
-}
-function GoToPopoverLabel(props: { accountName: string; vkMode: VkMode }) {
-  switch (props.vkMode) {
-    case VkMode.VkVideo:
-      return (
-        <p>
-          Показать аккаунт <AccountLabel accountName={props.accountName} /> в
-          VK?
-        </p>
-      );
-    case VkMode.Vk:
-      return (
-        <p>
-          Показать аккаунт <AccountLabel accountName={props.accountName} /> в VK
-          Видео?
-        </p>
-      );
-  }
 }
 
-function GoToPopoverActions(props: { goToVkVideo: () => void }) {
-  return (
-    <motion.div layout className={"grid grid-cols-1 items-center gap-2 w-full"}>
-      <Button
-        className={"font-medium"}
-        color={"secondary"}
-        endContent={<GoToIcon height={20} width={20} />}
-        variant={"solid"}
-        onPress={props.goToVkVideo}
-      >
-        Перейти
-      </Button>
-    </motion.div>
-  );
-}
 function AccountLabel(props: { accountName: string }) {
   return <span className={"font-bold text-nowrap"}>{props.accountName}</span>;
 }
